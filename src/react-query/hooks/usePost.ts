5fe52ea68ed0f6ed3661864e481118f9670f5e71
Sławomir Woryna <105556865+SJZenzo@users.noreply.github.com>
2023-04-30 17:11:49 +0200

    commit

---
+++ src/react-query/hooks/usePost.ts
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+interface PostQuery {
+  page: number;
+  pageSize: number;
+}
+
+const usePost = (query: PostQuery) =>
+  useQuery<Post[], Error>({
+    queryKey: ["posts", query],
+    queryFn: () =>
+      axios
+        .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+          params: {
+            _start: (query.page - 1) * query.pageSize,
+            _limit: query.pageSize,
+          },
+        })
+        .then((res) => res.data),
+    staleTime: 10 * 1000,
+    keepPreviousData: true,
+  });
+
+export default usePost;
